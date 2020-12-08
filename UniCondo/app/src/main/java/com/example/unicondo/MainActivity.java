89e63448238da1706cf3a089e626f37959e3549c
CleilsonAraújo <cleilson.cl997@gmail.com>
2020-12-07 21:58:01 -0300

    pequenos ajustes + orientações de instalação

--- UniCondo/app/src/main/java/com/example/unicondo/MainActivity.java
+++ UniCondo/app/src/main/java/com/example/unicondo/MainActivity.java
@@ -259,6 +259,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onRestart(){
         super.onRestart();
-        recreate();
+        txtEmail.setText("");
+        txtSenha.setText("");
     }
 }
